00185ef9f486cd4a86755928952c096a40d0dfee
Godson2611 <godson2611@gmail.com>
2024-11-07 08:30:31 +0530

    add two btn for Question page

--- frontend/src/components/Question.jsx
+++ frontend/src/components/Question.jsx
@@ -1,6 +1,11 @@
 const Question = () => {
   return (
-    <div>Question</div>
-  )
-}
-export default Question
+    <div className='container'>
+      <div className='d-flex gap-2 justify-content-end'>
+        <button className='btn btn-info text-white'>10 MCQ</button>
+        <button className='btn btn-info text-white'>2 Test</button>
+      </div>
+    </div>
+  );
+};
+export default Question;
