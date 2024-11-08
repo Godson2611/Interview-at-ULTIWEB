dfe261007763d95c14c3f15c1ae30bfc4468a7ab
Godson2611 <godson2611@gmail.com>
2024-11-08 11:04:52 +0530

    50 % task completed

--- frontend/src/components/Preview.jsx
+++ frontend/src/components/Preview.jsx
@@ -1,6 +1,29 @@
-const Preview = () => {
+import React from "react";
+
+const Preview = ({ questions, deleteQuestion }) => {
   return (
-    <div>Preview</div>
-  )
-}
-export default Preview
+    <div className='container'>
+      <h3>Preview Questions</h3>
+      {questions.map((q) => (
+        <div key={q.id} className='card my-3'>
+          <div className='card-body'>
+            <h5>{q.questionText}</h5>
+            <ul>
+              {q.options.map((opt) => (
+                <li key={opt.optionId}>{opt.optionText}</li>
+              ))}
+            </ul>
+            <button
+              className='btn btn-danger'
+              onClick={() => deleteQuestion(q.id)}
+            >
+              Delete
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Preview;
--- frontend/src/components/Question.jsx
+++ frontend/src/components/Question.jsx
@@ -1,72 +1,105 @@
 import React, { useState } from "react";
 
-const Question = () => {
-  const [questionType, setQuestionType] = useState("MCQ");
+const Question = ({ addQuestion, updateQuestion }) => {
+  const [question, setQuestion] = useState({
+    type: "MCQ",
+    questionText: "",
+    options: [],
+    continueTo: "",
+  });
 
-  const handleQuestionTypeChange = (type) => {
-    setQuestionType(type);
+  const handleQuestionChange = (e) => {
+    setQuestion({ ...question, [e.target.name]: e.target.value });
+  };
+
+  const handleOptionChange = (index, value) => {
+    const newOptions = question.options.map((option, idx) =>
+      idx === index ? { ...option, optionText: value } : option
+    );
+    setQuestion({ ...question, options: newOptions });
+  };
+
+  const addOption = () => {
+    setQuestion({
+      ...question,
+      options: [...question.options, { optionId: Date.now(), optionText: "" }],
+    });
+  };
+
+  const removeOption = (index) => {
+    setQuestion({
+      ...question,
+      options: question.options.filter((_, idx) => idx !== index),
+    });
+  };
+
+  const handleSave = () => {
+    if (question.id) {
+      updateQuestion(question);
+    } else {
+      addQuestion({ ...question, id: Date.now() });
+    }
+    setQuestion({
+      type: "MCQ",
+      questionText: "",
+      options: [],
+      continueTo: "",
+    });
   };
 
   return (
     <div className='container'>
-      <div className='d-flex gap-2 justify-content-end mt-5'>
-        <button className='btn btn-info text-white'>10 MCQ</button>
-        <button className='btn btn-info text-white'>2 Test</button>
-      </div>
-
       <div className='form'>
         <div className='shadow-sm p-3 mb-5 bg-body-tertiary rounded mt-4'>
-          <div className='question_input mt-3'>
-            <div className='d-flex justify-content-between'>
-              <h5>Question 1</h5>
-              <div className='dropdown'>
+          <h5>Question</h5>
+          <div className='form-floating mt-3'>
+            <textarea
+              className='form-control'
+              placeholder='Enter your Question'
+              name='questionText'
+              value={question.questionText}
+              onChange={handleQuestionChange}
+              style={{ height: "100px" }}
+            ></textarea>
+            <label>Enter your Question</label>
+          </div>
+          <div className='mt-4'>
+            <h5>Options</h5>
+            <button
+              type='button'
+              className='btn btn-info text-white mb-3'
+              onClick={addOption}
+            >
+              Add Option
+            </button>
+            {question.options.map((option, index) => (
+              <div
+                key={option.optionId}
+                className='d-flex gap-2 align-items-center mb-2'
+              >
+                <input
+                  type='text'
+                  className='form-control'
+                  value={option.optionText}
+                  onChange={(e) => handleOptionChange(index, e.target.value)}
+                />
                 <button
-                  className='btn btn-secondary dropdown-toggle'
                   type='button'
-                  data-bs-toggle='dropdown'
-                  aria-expanded='false'
+                  className='btn btn-danger'
+                  onClick={() => removeOption(index)}
                 >
-                  {questionType}
+                  x
                 </button>
-                <ul className='dropdown-menu'>
-                  <li>
-                    <button
-                      className='dropdown-item'
-                      onClick={() => handleQuestionTypeChange("MCQ")}
-                    >
-                      MCQ
-                    </button>
-                  </li>
-                  <li>
-                    <button
-                      className='dropdown-item'
-                      onClick={() => handleQuestionTypeChange("Test")}
-                    >
-                      Test
-                    </button>
-                  </li>
-                </ul>
               </div>
-            </div>
-            <div class='form-floating mt-3'>
-              <textarea
-                class='form-control'
-                placeholder='Enter your Question'
-                id='floatingTextarea'
-                style={{ height: "100px" }}
-              ></textarea>
-              <label for='floatingTextarea'>Enter your Question</label>
-            </div>
+            ))}
           </div>
-          <div className='question_options mt-4'>
-            <div className='d-flex gap-3 align-items-center'>
-              <h5>Options</h5>
-              <button type='button' className='btn btn-info text-white'>
-                Add Option
-              </button>
-            </div>
-            <div></div>
-          </div>
+          <button
+            type='button'
+            className='btn btn-primary'
+            onClick={handleSave}
+          >
+            Save Question
+          </button>
         </div>
       </div>
     </div>
